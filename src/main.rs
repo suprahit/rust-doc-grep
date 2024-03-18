3b51d31c2c34657155f5a13c41936e8393a80a01
Gabriel Nogueira <gabriel.nogueira21@outlook.com>
2024-03-17 22:46:47 -0300

    Collecting query and file path args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+use std::fs;
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+    let config = Config::new(&args);
+    let contents = fs::read_to_string(config.file_path).expect("Failed to read file");
+
+    println!("With text:\n{contents}");
 }
