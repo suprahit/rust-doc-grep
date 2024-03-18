0ac000e96c4de20621ea3c3434ab68f9e265b1d2
Gabriel Nogueira <gabriel.nogueira21@outlook.com>
2024-03-17 23:14:28 -0300

    Better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,18 @@
 use std::env;
-use std::fs;
+use std::process;
 
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Config {
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Config { query, file_path }
-    }
-}
+use rust_doc_grep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
-    let config = Config::new(&args);
-    let contents = fs::read_to_string(config.file_path).expect("Failed to read file");
 
-    println!("With text:\n{contents}");
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = rust_doc_grep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
