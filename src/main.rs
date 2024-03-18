dc35a65c17bb2e7893e300e0f88cab1686bd632e
Gabriel Nogueira <gabriel.nogueira21@outlook.com>
2024-03-18 00:06:50 -0300

    stdprintln! to eprintln!

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = rust_doc_grep::run(config) {
-        println!("{e}");
+        eprintln!("{e}");
         process::exit(1);
     }
 }
